cef44b350c3f6c04e4e66c7677513f518fbb5672
s9904320 <91059581+s9904320@users.noreply.github.com>
2022-01-16 22:25:21 +0800

    Add project files.

---
+++ homework2Remake/DeadAntCount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace homework2Remake
+{
+    public class DeatAnt
+    {
+        public static int DeadAntCount(string ants)
+        {
+            int deadAnt = 0;
+            int allAnt = 0;
+            int[] partDeadAnt = { 0, 0, 0 };
+
+            if (!String.IsNullOrEmpty(ants))
+            {
+                for (int i = 0; i < ants.Length; i++)
+                {
+                    if (ants.Substring(i, 1) == "a")
+                    {
+                        partDeadAnt[0] += 1;
+                    }
+                    if (ants.Substring(i, 1) == "n")
+                    {
+                        partDeadAnt[1] += 1;
+                    }
+                    if (ants.Substring(i, 1) == "t")
+                    {
+                        partDeadAnt[2] += 1;
+                    }
+                    if (i < ants.Length - 2)
+                    {
+                        if (ants.Substring(i, 3) == "ant")
+                        {
+                            allAnt++;
+                        }
+                    }
+                }
+                deadAnt = partDeadAnt.Max() - allAnt;
+
+                return deadAnt;
+            }
+            else
+            {
+                return 0;
+            }         
+        }
+    }
+}
---
+++ homework2RemakeTests/DeatAntTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using homework2Remake;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2Remake.Tests
+{
+    [TestClass()]
+    public class DeatAntTests
+    {
+        [TestMethod]
+        public void testNullOrEmpty()
+        {
+            Assert.AreEqual(0, DeatAnt.DeadAntCount(null));
+            Assert.AreEqual(0, DeatAnt.DeadAntCount(""));
+        }
+        [TestMethod]
+        public void testNoDead()
+        {
+            Assert.AreEqual(0, DeatAnt.DeadAntCount("ant ant ant ant"));
+        }
+        [TestMethod]
+        public void testNormal()
+        {
+            Assert.AreEqual(2, DeatAnt.DeadAntCount("ant anantt aantnt"));
+        }
+        [TestMethod]
+        public void testNotNormal()
+        {
+            Assert.AreEqual(3, DeatAnt.DeadAntCount("...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t"));
+        }
+    }
+}
